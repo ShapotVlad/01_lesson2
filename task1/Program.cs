82ed50013e187df94143a03ff2bfb692697aef89
Vladimir <vlnshapat@gmail.com>
2024-04-25 16:34:26 +0300

    add less2/task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[5];
+arr[0] = 21;
+arr[1] = 25;
+arr[2] = 121;
+arr[3] = 31;
+arr[4] = 45;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5] { 14, 52, 45, 56, 85, };
+
+int[] arr3 = {52,85,94,76,103};
