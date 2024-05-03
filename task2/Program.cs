2cb1eef5b7ee79b8275984c1ef22988b983095c4
Vladimir <vlnshapat@gmail.com>
2024-05-03 16:11:34 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// создать массив и заполнить его числами от 1 до N;
+
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < n)
+{
+    arr[i] = i + 1;
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
